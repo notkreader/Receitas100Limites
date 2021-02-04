ba1c7705baffa5724867a0ecec08834c7b295cfc
notkreader <undisclosed>
2021-02-04 00:35:27 +0000

    Update 03/02 e 04/02

--- scripts/script.js
+++ scripts/script.js
@@ -5,21 +5,66 @@
 
 window.onload = posts;
 */
-
 function AllRecipes() {
-    this.allRecipes = [];
+    this.recipesList = [];
+    this.size = 0;
 }
 
-function Recipe(name, confectionTime, ingredients, preparation, image) {
+AllRecipes.showRecipes = function(recipesList) {
+    recipesList = recipesList || AllRecipes.defaultValues;
+    if (recipesList.size > 0)
+        document.getElementById('recipes').innerHTML = recipesList.listRecipe();
+}
+
+AllRecipes.prototype.listRecipe = function() {
+    let str = '';
+    this.recipesList.forEach(recipe => {
+        if (str === '')
+            str = '<div id="recipe">';
+        else
+            str += '<div id="recipe">';
+        str += '<div id="recipe_image_div"> <img id="recipe_image" src="images/esparguete_camarao.png"> </div>';
+        str += '<label id="title">' + recipe.name + '</label><br><br>';
+        str += '<label id="main_word">Tempo de Confeção: </label><label id="desc">' + recipe.confectionTime + '</label><br><br>';
+        str += '<label id="main_word">Ingredientes:</label><br><label id="desc">';
+        recipe.ingredients.forEach(elem => {
+            str += elem + '<br>';
+        })
+        str += '</label><br><label id="main_word">Preparação:</label><br><label id="desc">';
+        recipe.preparation.forEach(elem => {
+            str += elem + '<br>';
+        })
+        str += '</label></div>';
+    })
+    return str;
+}
+
+AllRecipes.addRecipe = function(recipesList) {
+    let recipeType = document.getElementById('recipe_type').value;
+    let recipeName = document.getElementById('recipe_name').value;
+    let recipeConfectionTime = document.getElementById('recipe_time').value;
+    let recipeIngredients = document.getElementById('recipe_ingredients').value;
+    let recipePreparation = document.getElementById('recipe_preparation').value;
+    //let recipeImage = document.getElementById('').value;
+    recipesList.addRecipe(new Recipe(recipeType, recipeName, recipeConfectionTime, recipeIngredients, recipePreparation, 'abc'));
+    recipesList.size += 1;
+    AllRecipes.showRecipes(recipesList);
+}
+
+AllRecipes.prototype.addRecipe = function(recipe) {
+    this.recipesList.push(recipe);
+}
+
+AllRecipes.defaultValues = (new AllRecipes());
+
+let recipesList = AllRecipes.defaultValues;
+
+
+function Recipe(type, name, confectionTime, ingredients, preparation, image) {
+    this.type = type;
     this.name = name;
     this.confectionTime = confectionTime;
-    this.ingredients = ingredients;
-    this.preparation = preparation;
-    this.image = (image && validateImage(image)) ? race : 'No Image';
-}
-
-function validateImage(file) {
-    const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
-
-    return file && acceptedImageTypes.includes(file['type'])
+    this.ingredients = ingredients.replace(/\r\n/g, "\n").split("\n");
+    this.preparation = preparation.replace(/\r\n/g, "\n").split("\n");
+    this.image = image;
 }
