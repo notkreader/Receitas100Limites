5d771b5629a00479f7e9094e4781fbfb3e4bbaa7
notkreader <undisclosed>
2021-02-05 15:47:26 +0000

    Commit on terminal

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,3 @@
-/*let posts = function() {
-    const recipes = document.querySelectorAll("#recipe");
-    console.dir(recipes);
-}
-
-window.onload = posts;
-*/
 function AllRecipes() {
     this.recipesList = [];
     this.size = 0;
@@ -23,7 +16,8 @@ AllRecipes.prototype.listRecipe = function() {
             str = '<div id="recipe">';
         else
             str += '<div id="recipe">';
-        str += '<div id="recipe_image_div"> <img id="recipe_image" src="images/esparguete_camarao.png"> </div>';
+        str += '<div id="recipe_image_div"> <img id="recipe_image" src="' + recipe.image + '"> </div>';
+        //str += '<div id="recipe_image_div"> <img id="recipe_image" src="images/esparguete_camarao.png"> </div>';
         str += '<label id="title">' + recipe.name + '</label><br><br>';
         str += '<label id="main_word">Tempo de Confeção: </label><label id="desc">' + recipe.confectionTime + '</label><br><br>';
         str += '<label id="main_word">Ingredientes:</label><br><label id="desc">';
@@ -45,8 +39,9 @@ AllRecipes.addRecipe = function(recipesList) {
     let recipeConfectionTime = document.getElementById('recipe_time').value;
     let recipeIngredients = document.getElementById('recipe_ingredients').value;
     let recipePreparation = document.getElementById('recipe_preparation').value;
-    //let recipeImage = document.getElementById('').value;
-    recipesList.addRecipe(new Recipe(recipeType, recipeName, recipeConfectionTime, recipeIngredients, recipePreparation, 'abc'));
+    //let recipeImage = document.getElementById('add_image').value;
+
+    recipesList.addRecipe(new Recipe(recipeType, recipeName, recipeConfectionTime, recipeIngredients, recipePreparation, 'image'));
     recipesList.size += 1;
     AllRecipes.showRecipes(recipesList);
 }
@@ -67,4 +62,60 @@ function Recipe(type, name, confectionTime, ingredients, preparation, image) {
     this.ingredients = ingredients.replace(/\r\n/g, "\n").split("\n");
     this.preparation = preparation.replace(/\r\n/g, "\n").split("\n");
     this.image = image;
+}
+
+
+
+// Código para dar print nos compos do login
+printLogin = function() {
+    let mail = document.getElementById("mail_txt");
+    let pass = document.getElementById("pass_txt");
+
+    console.log(`Email: ${mail.value} | Password: ${pass.value}`);
+}
+
+// Código para o menu andar para baixo
+window.onscroll = function() {
+    var navbar = document.getElementById("menu");
+
+    if (window.pageYOffset >= 200) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+};
+
+// Código para a avaliação de 1 a 5 em estrelas
+document.addEventListener('DOMContentLoaded', function() {
+    let stars = document.querySelectorAll('.star');
+    stars.forEach(function(star) {
+        star.addEventListener('click', function(ev) {
+            let span = ev.currentTarget;
+            let stars = document.querySelectorAll('.star');
+            let match = false;
+            let num = 0;
+            stars.forEach(function(star, index) {
+                if (match) {
+                    star.classList.remove('rated');
+                } else {
+                    star.classList.add('rated');
+                }
+                //are we currently looking at the span that was clicked
+                if (star === span) {
+                    match = true;
+                    num = index + 1;
+                }
+            });
+            document.querySelector('.stars').setAttribute('data-rating', num);
+        });
+    });
+
+    let rating = parseInt(document.querySelector('.stars').getAttribute('data-rating'));
+    let target = stars[rating - 1];
+    target.dispatchEvent(new MouseEvent('click'));
+});
+
+// Código que dá print no rating recebido pelo cliente
+printStartClassification = function(number) {
+    console.log(`${ number }`);
 }
